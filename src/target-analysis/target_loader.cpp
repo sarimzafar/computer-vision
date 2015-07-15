a21d333ddbf35018fb94d4bcadfddd32d14dfc72
Christopher Hajduk <chrishajduk84@hotmail.com>
2015-07-14 23:02:25 -0400

    JSON target file specifications

--- src/target-analysis/target_loader.cpp
+++ src/target-analysis/target_loader.cpp
@@ -1,2 +1,35 @@
 #include "json/json.h"
 
+/*
+* JSON Structure
+* -One file per target type
+* {
+*      "parameters": {
+                     <parameter/category>: {
+                              "mean":{
+                                      "enabled":<True/False>    ->Use this only as an override, the target_analyzer will have jurisdiction over 
+                                      "value":<value>
+                                      "confidence":<value>      -> Integer value ranging from 0 to 100
+                              }
+                              "mode":{
+                                      "enabled":<True/False>    ->Use this only as an override, the target_analyzer will have jurisdiction over 
+                                      "value":<value>
+                                      "confidence":<value>      -> Integer value ranging from 0 to 100
+                              }
+                              "std":{
+                                      "enabled":<True/False>
+                                      "value":<value>
+                                      "confidence":<value>      -> Integer value ranging from 0 to 100
+                              }
+                              "distribution":{                  ->Examples like gaussian/binomial/etc
+                                      "enabled":<True/False>
+                                      "value":<value>
+                                      "confidence":<value>      -> Integer value ranging from 0 to 100
+                              }
+                           }
+                     
+       } 
+* }
+*/
+
+void targetLoader::
